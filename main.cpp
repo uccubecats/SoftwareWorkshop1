f26980bbabe7a38ceec20ce67431cd6761053918
uccubecats <uccubecats@gmail.com>
2015-10-25 14:25:13 -0400

    Added CMake Files and Main

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "util/Util.h"
+#include <stdio.h>
+
+int main(int argn, char** argv){
+	printf("From Main: %i \n", testFunction(20));
+	return 0;
+}
---
+++ util/Util.c
@@ -0,0 +1,7 @@
+#include "Util.h"
+#include <stdio.h>
+
+int testFunction(int number) {
+	printf("From Library: %i \n", number);
+	return 7;
+}
